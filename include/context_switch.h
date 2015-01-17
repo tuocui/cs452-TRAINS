c23e07ac09bb46f8f6b216be68ebd3a64098c5c4
Wilson_Zheng <z26zheng@uwaterloo.ca>
2015-01-16 22:08:37 -0500

    context switch debugging

--- include/context_switch.h
+++ include/context_switch.h
@@ -1,4 +1,13 @@
+#ifndef __CONTEXT_SWITCH_H__
+#define __CONTEXT_SWITCH_H__
+
+
 #include "bwio.h"
 
 void print_pc();
 
+void kernel_exit(int retval, int sp, int spsr);
+
+void init_kernelentry();
+
+#endif
--- src/main_func.c
+++ src/main_func.c
@@ -1,10 +1,37 @@
 #include "kernel.h"
 #include "context_switch.h"
 
+void second_task() {
+  bwprintf(COM2, "SECONDUSER TASK!!\n\r");
+  asm("swi 0");
+}
+
+
+void first_task() {
+  bwprintf(COM2, "FIRST USER TASK!!\n\r");
+  //asm("swi 0");
+}
 
 int main(int argc, char *argv[]) {
-  bwprintf(COM2, "Hellow\n\r");
+  int initbuf[9999];
+  bwprintf( COM2, "Enter main" );
+
   print_pc();
+  init_kernelentry();
+
+  bwprintf(COM2, "Hellow\n\r");
+
+  char ch = 'A';
+  int * sp;
+  sp = initbuf + 1000;
+  bwprintf(COM2, "We set char: %c\n\r", ch);
+  int * lr = (sp - 14);
+  *lr = (int)&first_task;
+
+  kernel_exit(1, (int)sp, 0xd0);
   bwprintf(COM2, "\n\rGoodbye\n\r");
   return 0;
 }
+
+
+
