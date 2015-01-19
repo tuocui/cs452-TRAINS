c78fab86c7f391178d45d402c875450b06f72a1d
Wilson_Zheng <z26zheng@uwaterloo.ca>
2015-01-19 14:11:23 -0500

    everything works

--- src/kernel.c
+++ src/kernel.c
@@ -26,9 +26,9 @@ int main(int argc, char *argv[]) {
   //);
   //redboot_lr = lr_reg;
   
-  //global_context_t gc;
-  //tds_init(&gc);
-  //init_kernelentry();
+  global_context_t gc;
+  tds_init(&gc);
+  init_kernelentry();
 
   //task_descriptor_t *first_td = tds_create_td(&gc, 1, (int*)( (int) &first_task + REDBOOT_OFFSET));
  // task_descriptor_t *second_td = tds_create_td(&gc, 1, (int*)( (int) &second_task + REDBOOT_OFFSET));
@@ -37,38 +37,38 @@ int main(int argc, char *argv[]) {
 
   assert(1==2, "this is a msg");
 
- // register unsigned int *new_sp_reg asm("r0"); // absolute 
- // unsigned int *new_sp;
+ register unsigned int *new_sp_reg asm("r0"); // absolute 
+ unsigned int *new_sp;
 
- // int i = 0;
- // while (1){
- //   task_descriptor_t *first_td = tds_create_td(&gc, 1, (int*)( (int) &first_task + REDBOOT_OFFSET));
- //   if (first_td == NULL) {
- //     break;    
- //   }
- //   kernel_exit(1, first_td->sp, 0xd0);
- //   asm volatile(
- //     "mov %0, r1\n\t" 
- //   : "+r"(new_sp_reg)
- //   );
- //   new_sp = new_sp_reg;
- //   bwprintf( COM2, "new_sp1:%d\r\n", new_sp ); 
- //   first_td->sp = new_sp;
+ int i = 0;
+ while (1){
+   task_descriptor_t *first_td = tds_create_td(&gc, 1, (int*)( (int) &first_task + REDBOOT_OFFSET));
+   if (first_td == NULL) {
+     break;    
+   }
+   kernel_exit(1, first_td->sp, 0xd0);
+   asm volatile(
+     "mov %0, r1\n\t" 
+   : "+r"(new_sp_reg)
+   );
+   new_sp = new_sp_reg;
+   bwprintf( COM2, "new_sp1:%d\r\n", new_sp ); 
+   first_td->sp = new_sp;
 
- //   bwprintf( COM2, "i:%d\r\n", i ); 
+   bwprintf( COM2, "i:%d\r\n", i ); 
 
- //   int test;
- //   bwprintf( COM2, "test_addr:%d\r\n", &test ); 
- //   i++;  
- // }
+   int test;
+   bwprintf( COM2, "test_addr:%d\r\n", &test ); 
+   i++;  
+ }
 
- // bwprintf(COM2, "\n\rGoodbye\n\r");
- //
- // //lr_reg = redboot_lr;
- // //asm volatile(
- // //  "mov lr, %0\n\t" 
- // //: "+r"(lr_reg)
- // //);
+ bwprintf(COM2, "\n\rGoodbye\n\r");
+
+ //lr_reg = redboot_lr;
+ //asm volatile(
+ //  "mov lr, %0\n\t" 
+ //: "+r"(lr_reg)
+ //);
   return 0;
 }
 
