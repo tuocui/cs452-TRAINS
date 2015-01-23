79411af94a717540dfb7f9db2151bc71251a8cab
t2cui <t2cui@>
2015-01-23 17:17:51 -0500

    removed idx from user outputs

--- src/user_task.c
+++ src/user_task.c
@@ -8,11 +8,9 @@ void a1_user_task( ){
   unsigned int parent_tid;
   my_tid = MyTid( );
   parent_tid = MyParentTid( );
-  bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", TID_IDX(my_tid),
-      TID_IDX(parent_tid) );
+  bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", my_tid, parent_tid );
   Pass( );
-  bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", TID_IDX(my_tid),
-      TID_IDX(parent_tid) );
+  bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", my_tid, parent_tid );
   Exit( );
 }
 #endif /* A1 */
@@ -22,13 +20,13 @@ void first_user_task( ){
 #ifdef A1
   int created_tid;
   created_tid = Create( 10, &a1_user_task );
-  bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", TID_IDX(created_tid));
-  created_tid = Create( 1, &a1_user_task );
-  bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", TID_IDX(created_tid));
-  created_tid = Create( 1, &a1_user_task );
-  bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", TID_IDX(created_tid));
+  bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", created_tid);
   created_tid = Create( 10, &a1_user_task );
-  bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", TID_IDX(created_tid));
+  bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", created_tid);
+  created_tid = Create( 1, &a1_user_task );
+  bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", created_tid);
+  created_tid = Create( 1, &a1_user_task );
+  bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", created_tid);
 #endif /* A1 */
 
   bwprintf( COM2, "First: exiting\n\r");
