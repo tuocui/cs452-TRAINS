910316e7074c05d4d59a7b3aa4e5e8de09e17287
Wilson_Zheng <z26zheng@uwaterloo.ca>
2015-01-24 13:47:40 -0500

    removed + 1 on index, figured we need td's position of the array later on so let's remove the redundant + 1

--- src/user_task.c
+++ src/user_task.c
@@ -9,8 +9,10 @@ void a1_user_task( ){
   my_tid = MyTid( );
   parent_tid = MyParentTid( );
   bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", my_tid, parent_tid );
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(my_tid), TID_GEN(my_tid));
   Pass( );
   bwprintf( COM2, "My TID: %d, Parent TID: %d\n\r", my_tid, parent_tid );
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(my_tid), TID_GEN(my_tid));
   Exit( );
 }
 #endif /* A1 */
@@ -18,15 +20,25 @@ void a1_user_task( ){
 
 void first_user_task( ){
 #ifdef A1
+  int first_tid = MyTid( );
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(first_tid), TID_GEN(first_tid));
+
   int created_tid;
   created_tid = Create( 10, &a1_user_task );
   bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", created_tid);
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(created_tid), TID_GEN(created_tid));
+
   created_tid = Create( 10, &a1_user_task );
   bwprintf( COM2, "Created: %d, Priority: Lower  than First\n\r", created_tid);
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(created_tid), TID_GEN(created_tid));
+
   created_tid = Create( 1, &a1_user_task );
   bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", created_tid);
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(created_tid), TID_GEN(created_tid));
+
   created_tid = Create( 1, &a1_user_task );
   bwprintf( COM2, "Created: %d, Priority: Higher than First\n\r", created_tid);
+  debug( "TID_IDX: %d, TID_GEN: %d", TID_IDX(created_tid), TID_GEN(created_tid));
 #endif /* A1 */
 
   bwprintf( COM2, "First: exiting\n\r");
